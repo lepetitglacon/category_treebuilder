0bbfdb798489cafabb826365679d94336d20f9a5
petitglacon <58629249+lepetitglacon@users.noreply.github.com>
2022-10-19 18:43:55 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Category TreeBuilder',
+    'title' => 'Category Tree Builder',
     'description' => 'Makes category tree management easier for contributors. Adds csv import/export features as well as built-in text builder.',
-    'category' => 'plugin',
+    'category' => 'module',
     'author' => 'petitglacon',
-    'author_email' => '',
+    'author_email' => 'contact@petitglacon.com',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '0.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
