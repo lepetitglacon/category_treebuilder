d69abbbb9eb4ebaa7c18843475ae3ffd0046b7ac
Esteban Gagneur <esteban.gagneur@cameros.fr>
2022-10-29 22:30:40 +0200

    publish v1.0.1 on TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Category Tree Builder',
-    'description' => 'Makes category tree management easier for contributors. Adds csv import/export features as well as built-in text builder.',
+    'description' => 'Build you\'re category tree with a built-in text editor and more.',
     'category' => 'module',
     'author' => 'petitglacon',
     'author_email' => 'contact@petitglacon.com',
-    'state' => 'alpha',
+    'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '0.0.1',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
